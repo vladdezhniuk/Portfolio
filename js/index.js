1cb3474e1a33aa245c0c1d11838a1b217196db5a
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-02-28 11:37:49 +0200

    adaptivity fixed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav-mobile');
+
+burger.onclick = function() {
+  this.classList.toggle('change');
+
+  if (nav.style.visibility === 'visible') {
+    nav.style.visibility = 'hidden';
+    nav.style.bottom = '100vh';
+  } else {
+    nav.style.visibility = 'visible';
+    nav.style.bottom = '0';
+  };
+};
+
 let hover = document.getElementsByClassName('greeting__heading');
+
 for (let i = 0; i < hover.length; i++) {
     hover[i].innerHTML = hover[i].innerHTML.replace(/(.)/g, '<span>$1</span>');
 }
