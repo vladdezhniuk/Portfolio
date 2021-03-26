5093ab9e0a83c6dd9b8e4e08e4d5c09b21259c05
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-03-27 00:03:31 +0200

    animation

--- js/index.js
+++ js/index.js
@@ -18,3 +18,23 @@ let hover = document.getElementsByClassName('greeting__heading');
 for (let i = 0; i < hover.length; i++) {
     hover[i].innerHTML = hover[i].innerHTML.replace(/(.)/g, '<span>$1</span>');
 }
+
+
+let translate = document.querySelector('.animation__non-reverse');
+let translateReverse = document.querySelector('.animation__reverse');
+let translateItem = document.querySelectorAll('.animation__non-reverse i');
+let translateReverseItem = document.querySelectorAll('.animation__reverse i')
+
+
+
+
+document.addEventListener('mousemove', (event) => {
+
+  let mouseY = event.clientY;
+  let mouseX = event.clientX;
+
+  translate.style.top =  (mouseY / 10) + 'px';
+  translate.style.left =  (mouseX / 10) + 'px';
+  translateReverse.style.top = (-mouseY / 10) + 'px';
+  translateReverse.style.left = (-mouseX / 10) + 'px';
+})
