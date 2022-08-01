298034beca3ba8138eb3f24e0cfb106e6bca074f
vladdezhniuk <vladdezhniuk@gmail.com>
2022-08-02 00:58:38 +0300

    heading second parameter added

--- src/app/components/common/Heading/index.tsx
+++ src/app/components/common/Heading/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './index.scss';
 
-export const Heading: React.FC<{ message: string }> = ({ message }) =>
-    <h1 className="heading">
+export const Heading: React.FC<{ message: string, className?: string }> = ({ message, className = '' }) =>
+    <h1 className={`heading ${className}`}>
         {message.split('').map((letter: string, index: number) =>
             <>
                 <span
-                    className="heading__letter"
+                    className={`heading__letter ${className && `${className}__letter`}`}
                     key={letter}
                     datatype={letter}
                     style={{ animationDelay: `${0.1 * index}s` }}
