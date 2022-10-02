66f2d12c01d06dea7c00a79fa463f6264779a5bf
vladdezhniuk <vladdezhniuk@gmail.com>
2022-10-02 21:40:58 +0300

    contact form added, map added

--- src/app/components/Contact/Form/index.tsx
+++ src/app/components/Contact/Form/index.tsx
@@ -1,30 +1,67 @@
-import React from 'react';
+import { FormEvent, useState } from 'react';
+
 
 import './index.scss';
 
-export const Form = () =>
-    <form className="form">
-        <input
-            type="text"
-            placeholder="Name"
-            className="form__name"
-        />
-        <input
-            type="email"
-            placeholder="Enter email"
-            className="form__email"
-        />
-        <input
-            type="text"
-            placeholder="Message"
-            className="form__message"
-        />
-        <button
-            type="submit"
-            className="form__submit"
-            datatype="Hit me up"
-        >
-                Hit me up
-        </button>
-    </form>;
+export const Form = () => {
+    const FORM_ENDPOINT = 'https://public.herotofu.com/v1/20b89e10-426a-11ed-9b17-6fdf7f94f506';
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
 
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            await fetch(FORM_ENDPOINT, {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    message
+                }),
+            })
+        } catch (error) { }
+
+    }
+    return (
+        <form
+            className="form"
+            action={FORM_ENDPOINT}
+            method="post"
+            onSubmit={handleSubmit}
+        >
+            <input
+                type="text"
+                placeholder="Name"
+                className="form__name"
+                id='name'
+                onChange={e => setName(e.target.value)}
+            />
+            <input
+                type="email"
+                placeholder="Enter email"
+                className="form__email"
+                id='email'
+                onChange={e => setEmail(e.target.value)}
+            />
+            <input
+                type="text"
+                id="message"
+                placeholder="Message"
+                className="form__message"
+                onChange={e => setMessage(e.target.value)}
+            />
+            <button
+                type="submit"
+                className="form__submit"
+                datatype="Hit me up"
+            >
+                Hit me up
+            </button>
+        </form>
+    )
+}
