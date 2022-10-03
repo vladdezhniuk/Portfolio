cb7b6511172e477cd1570f220c9238a0e5489ece
vladdezhniuk <vladdezhniuk@gmail.com>
2022-10-03 13:57:49 +0300

    navbar, home, about pages mobile versions

--- src/app/components/common/DelayedLink/index.tsx
+++ src/app/components/common/DelayedLink/index.tsx
@@ -2,23 +2,31 @@ import React, { useContext } from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { LoaderContext } from '@/app/context/loader';
 
-class DelayedLinkProps {
-    constructor(
-        public to: string,
-        public className: string,
-        public children: React.ReactNode,
-        public datatype: string
-    ) { }
+interface DelayedLinkProps {
+    to: string;
+    className: string;
+    children: React.ReactNode;
+    datatype: string;
+    setIsNavbarVisible?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 /** Delays route change for improve loader behavior */
-export const NavLinkWithDelay: React.FC<DelayedLinkProps> = ({ to, className, children, datatype }) => {
+export const NavLinkWithDelay: React.FC<DelayedLinkProps> = (
+    {
+        to,
+        className,
+        children,
+        datatype,
+        setIsNavbarVisible,
+    }) => {
     const { isLoading, setIsLoading } = useContext(LoaderContext);
     const location = useLocation();
     const navigate = useNavigate();
 
     /** Handles route change, changes loader context */
     const changeRoute = (e: any) => {
+        setIsNavbarVisible &&
+            setIsNavbarVisible(false);
         if (location.pathname === to || isLoading) { return; }
 
         e.preventDefault();
--- src/app/components/common/Navbar/index.tsx
+++ src/app/components/common/Navbar/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { RoutesConfig } from '@/app/routes';
-import './index.scss';
+import React, { useState } from 'react';
+import burger from '@static/images/navbar/burger.svg';
+import cross from '@static/images/navbar/cross.svg';
 import { NavLinkWithDelay } from '../DelayedLink';
+
+import { RoutesConfig } from '@/app/routes';
+
+
+import './index.scss';
 
 class NavigationLink {
     constructor(
@@ -12,11 +16,16 @@ class NavigationLink {
 }
 
 export const Navbar = () => {
+    const [isNavbarVisible, setIsNavbarVisible] = useState(false);
     const navLinks = [
         new NavigationLink('About', RoutesConfig.About.path),
         new NavigationLink('Projects', RoutesConfig.Projects.path),
         new NavigationLink('Contact me', RoutesConfig.Contact.path),
     ];
+
+    const changeNavbarVisibility = () => {
+        setIsNavbarVisible(prev => !prev);
+    };
 
     return (
         <nav className="nav">
@@ -24,18 +33,30 @@ export const Navbar = () => {
                 className="nav__link nav__logo"
                 to={RoutesConfig.Home.path}
                 datatype="vladdezhniuk"
+                setIsNavbarVisible={setIsNavbarVisible}
             >
                 vladdezhniuk
             </NavLinkWithDelay>
-            <ul className="nav__list">
+            <ul className={`nav__list${isNavbarVisible ? '-active' : ''}`}>
                 {navLinks.map((link: NavigationLink) =>
                     <li className="nav__item" key={link.label} >
-                        <NavLinkWithDelay to={link.url} className="nav__link" datatype={link.label} >
+                        <NavLinkWithDelay
+                            to={link.url}
+                            className="nav__link"
+                            datatype={link.label}
+                            setIsNavbarVisible={setIsNavbarVisible}
+                        >
                             {link.label}
                         </NavLinkWithDelay>
                     </li>
                 )}
             </ul>
+            <img
+                src={isNavbarVisible ? cross : burger}
+                alt="navbar control"
+                className="nav__control"
+                onClick={changeNavbarVisibility}
+            />
         </nav>
     );
 };
