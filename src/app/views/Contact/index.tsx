96ccf83202d03bb552482176839858da5dbfbcfe
vladdezhniuk <vladdezhniuk@gmail.com>
2022-10-04 20:46:39 +0300

    restyled

--- src/app/views/Contact/index.tsx
+++ src/app/views/Contact/index.tsx
@@ -1,33 +1,46 @@
 
-import Map from 'google-map-react';
 import { Heading } from '@/app/components/common/Heading';
 import { Form } from '@/app/components/Contact/Form';
 
-import { theme } from '@/app/contants/map';
+import { Github, Linkedin, Telegram } from '@/app/static/images/contact/icons';
 
 import './index.scss';
 
+class SocialLink {
+    constructor(
+        public url: string,
+        public icon: JSX.Element
+    ) { }
+}
+
 export const Contact = () => {
-    const LOCATION_URL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184551.80858327696!2d-79.51814114754762!3d43.71840381103114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20ON%2C%20Canada!5e0!3m2!1sen!2spl!4v1664729208662!5m2!1sen!2spl';
+    const socialLinks = [
+        new SocialLink('https://github.com/vladdezhniuk', <Github />),
+        new SocialLink('https://www.linkedin.com/in/vladdezhniuk/', <Linkedin />),
+        new SocialLink('https://t.me/vladdezhniuk', <Telegram />),
+    ];
 
     return (
         <section className="contact">
-            <div className="contact__form-area">
-                <Heading message="Contact me" className="contact__heading" />
+            <Heading message="Contact me" className="contact__heading" />
+            <ul className="contact__list">
+                {socialLinks.map((link: SocialLink) =>
+                    <li className="contact__item">
+                        <a
+                            className="contact__link"
+                            href={link.url}
+                            target="_blank"
+                        >
+                            {
+                                link.icon
+                            }
+                        </a>
+                    </li>
+                )}
+            </ul>
+            <div className="contact__wrapper">
                 <Form />
             </div>
-            <div className="contact__map">
-                <Map
-                    bootstrapURLKeys={{ key: 'AIzaSyCD6psS1pUr3s0a81MgG6LaApXXO0S2GNE' }}
-                    defaultZoom={9}
-                    options={{ styles: theme }}
-                    defaultCenter={{
-                        lat: 43.7,
-                        lng: -79.5,
-                    }}
-                />
-            </div>
-
         </section>
     );
 };
