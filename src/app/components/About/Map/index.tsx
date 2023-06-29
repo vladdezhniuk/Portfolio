bba92255c57570bec099b8518d397402c10099f1
vladdezhniuk <vladdezhniuk@gmail.com>
2023-06-30 02:18:23 +0300

    map added

---
+++ src/app/components/About/Map/index.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api"
+
+import { Loader } from '../../common/Loader';
+
+import './index.scss';
+
+export const Map = () => {
+    const [zoom, setZoom] = useState(3);
+    const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.MAPS_API_KEY! });
+
+    const position = useMemo(() => ({ lng: 22.56, lat: 51.25 }), []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setZoom(6)
+        }, 1000);
+    }, []);
+
+    return (
+        <div className='map'>
+            {isLoaded
+                ?
+                <GoogleMap
+                    zoom={zoom}
+                    center={position}
+                    mapContainerClassName='map__container'
+                    options={{ mapId: '2889f91c543c823d' }}
+                >
+                    <Marker position={position} />
+                </GoogleMap>
+                :
+                <Loader spinnerSize='50px' />
+            }
+        </div>
+    )
+}
--- src/app/pages/About/index.tsx
+++ src/app/pages/About/index.tsx
@@ -1,12 +1,13 @@
 import { Description } from '@/app/components/About/Description';
+import { Map } from '@/app/components/About/Map';
 
 import './index.scss';
 
 export const About: React.FC = () => {
-
     return (
         <section className="about">
             <Description />
+            <Map />
         </section>
     );
 };
